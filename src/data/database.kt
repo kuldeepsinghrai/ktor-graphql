0abd269a18b63e3c16ac1d4fa696e26b2d0eaec4
KuldeepSinghRai <imkuldeepsinghrai@gmail.com>
2023-07-22 13:26:31 +0530

    database added

---
+++ src/data/database.kt
@@ -0,0 +1,9 @@
+package com.kuldeepsinghrai.data
+
+import com.kuldeepsinghrai.models.Dessert
+
+val desserts = mutableListOf(
+    Dessert(id = "1", name = "Chocolate Cake", description = "Cake", imageUrl = ""),
+    Dessert(id = "2", name = "Oreo Cake", description = "Sweet", imageUrl = ""),
+    Dessert(id = "3", name = "Apple Pie", description = "Tart", imageUrl = "")
+)
