eaca5b8b4dd7855d7a296a7ba0b9eef8a3e84246
KuldeepSinghRai <imkuldeepsinghrai@gmail.com>
2023-07-22 14:00:43 +0530

    Dessert Queries and Mutations

--- src/graphql/DessertSchema.kt
+++ src/graphql/DessertSchema.kt
@@ -1,28 +1,54 @@
 package com.kuldeepsinghrai.graphql
 
 import com.apurebase.kgraphql.schema.dsl.SchemaBuilder
+import com.kuldeepsinghrai.data.desserts
 import com.kuldeepsinghrai.models.Dessert
 import com.kuldeepsinghrai.models.DessertInput
 import com.kuldeepsinghrai.respository.DessertRepository
 
-fun SchemaBuilder.dessertSchema(){
+fun SchemaBuilder.dessertSchema() {
     val repository = DessertRepository()
 
     inputType<DessertInput> {
         description = "The input of the dessert without the identifier"
     }
 
-    type<Dessert>(){
+    type<Dessert>() {
         description = "Dessert object with attributes name, description and imageurl"
     }
 
-    query("dessert"){
-        resolver {dessertId:String->
+    query("dessert") {
+        resolver { dessertId: String ->
             try {
                 repository.getById(dessertId)
-            }catch (e:Exception){
+            } catch (e: Exception) {
                 null
             }
         }
     }
+
+    query("desserts") {
+        resolver { ->
+            try {
+                repository.getAll();
+            } catch (e: Exception) {
+                emptyList<Dessert>()
+            }
+        }
+    }
+
+    mutation("createDessert") {
+        description = "Create a new dessert"
+        resolver { dessertInput: DessertInput ->
+            try {
+                val uid = java.util.UUID.randomUUID().toString()
+                val dessert = Dessert(uid, dessertInput.name, dessertInput.description, dessertInput.description)
+                repository.add(dessert)
+            } catch (e: Exception) {
+                null
+            }
+        }
+    }
+
+
 }
