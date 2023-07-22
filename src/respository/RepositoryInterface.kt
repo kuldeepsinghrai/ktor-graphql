30f17687355291f3c10a0348a854359a4a8f3240
KuldeepSinghRai <imkuldeepsinghrai@gmail.com>
2023-07-22 13:30:28 +0530

    RepositoryInterface added

---
+++ src/respository/RepositoryInterface.kt
@@ -0,0 +1,10 @@
+package com.kuldeepsinghrai.respository
+
+interface RepositoryInterface<T>{
+    fun getById(id:String):T
+    fun getAll():List<T>
+    fun delete(id: String):Boolean
+    fun add(entry:T):T
+    fun update(entry: T):T
+
+}
